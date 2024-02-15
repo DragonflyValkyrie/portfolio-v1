424551eef910de8a8c17496234c03c81d8f99e49
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2024-02-15 14:50:54 -0500

    Added whoami typing and stylized bar for about me

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import scrollingSkills from './modules/scrolling-skills';
 import projectScrolling from './modules/projects-scroller';
+import typing from './modules/typing';
 
 scrollingSkills();
 projectScrolling();
+typing();
