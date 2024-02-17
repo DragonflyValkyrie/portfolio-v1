c98f0acc4e6d58d6ec5ca4c2184df6a994e593dd
DragonflyValkyrie <148598429+DragonflyValkryrie@users.noreply.github.com>
2024-02-16 19:17:42 -0500

    deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import scrollingSkills from './modules/scrolling-skills';
-import projectScrolling from './modules/projects-scroller';
-import typing from './modules/typing';
-
-scrollingSkills();
-projectScrolling();
-typing();
